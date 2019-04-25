7699e6c9b741da686ffa468269e9ec0a41fc8a3f
Arham J. Siddiqui <arhamjsiddiqui@gmail.com>
2019-04-25 09:23:00 -0500

    Initial commit

---
+++ Cryptext/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cryptext
+//
+//  Created by Siddiqui, Arham J on 4/25/19.
+//  Copyright © 2019 Siddiqui, Arham J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
